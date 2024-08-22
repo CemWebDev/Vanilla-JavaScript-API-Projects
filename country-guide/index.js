3b361b8a6c327a5b860407402951e484ec132705
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-22 16:27:19 +0300

    new commit

---
+++ country-guide/index.js
@@ -0,0 +1,16 @@
+/* const apiURL = "https://restcountries.com/v3.1/all";
+
+fetch(apiURL)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("There has been a problem with your fetch operation:", error);
+  });
+ */
