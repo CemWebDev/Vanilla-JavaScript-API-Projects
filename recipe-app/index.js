0391e07b4b15bc656d0a64491738d2aeaf94acbe
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-26 15:48:23 +0300

    update js

--- recipe-app/index.js
+++ recipe-app/index.js
@@ -1,9 +1,11 @@
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
+const resultContainer = document.querySelector(".result-container");
 const API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 const searchForMeal = () => {
-  console.log(searchInput.value);
+  const userInput = searchInput.value.trim();
+  fetchData(userInput);
 };
 
 const isInputValid = () => searchInput.value.trim() !== "";
@@ -12,23 +14,22 @@ const cleanInput = () => (searchInput.value = "");
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
   if (isInputValid()) {
-    console.log(searchInput.value);
     cleanInput();
+    searchForMeal();
     return;
   }
   alert("PLease enter a dish name.");
 });
 
-/* const fetchData = async () => {
+const fetchData = async (userInput) => {
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch(API_URL + userInput);
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error("Network response was not ok.");
     }
-    const data = response.json();
+    const data = await response.json();
     console.log(data);
   } catch (error) {
-    console.error("Error", error);
+    console.error("Error fetching data.");
   }
 };
-fetchData(); */
